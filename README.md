c1cbd73d6947e6222c4f41ac97fa1b898d15f787
Ruchi Upadhyay <uparuchi@gmail.com~>
2023-10-06 01:56:26 +0530

    update

---
+++ README.md
@@ -0,0 +1,37 @@
+# Quiz-App
+
+![Quiz Screenshot](quiz-screenshot.png)
+
+This is a simple interactive quiz web application created using HTML, CSS, and JavaScript. It features multiple-choice questions, scoring, and a timer for each question.
+
+## Table of Contents
+- [Features](#features)
+- [Tech Stack](#tech-stack)
+- [Installation and Setup Instructions](#Installation-and-Setup-Instructions)
+- [Getting Started](#Getting-Started)
+
+
+## Features
+
+- Multiple-choice questions with four answer options for each question.
+- Interactive user interface with hover and click effects on buttons.
+- Scoring system to keep track of the user's score.
+- Timer for each question to limit the time for answering.
+- Ability to navigate through questions using the "Next" button.
+- Option to restart the quiz after completing it.
+
+## **Tech stacks used :**
+To create this clone following Tech stack is used by contributors.  
+<br>
+<img src="https://img.shields.io/badge/html-E34F26?style=for-the-badge&logo=html5&logoColor=white"/>
+<img src="https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white"/>
+<img src="https://img.shields.io/badge/JavaScript-323330?style=for-the-badge&logo=javascript&logoColor=F7DF1E"/>
+
+## Installation and Setup Instructions
+Clone down this repository using this <a href="https://github.com/warriorruchi/Quiz-app">link</a> 
+<hr/>
+
+## Getting Started
+To run this project locally, follow these steps:
+1.**Install project**: Clone this repository to your local machine using Git.
+2.**Start the development server**: Copy the index.html path.
--- index.js
+++ index.js
@@ -1,45 +1,45 @@
-const questions =[
+const questions = [
 
     {
         question: "Which is largest animal in the world?",
-        answers:[
-            {text:"Shark",correct:false},
-            {text:"Blue Whale",correct:true},
-            {text:"Elephant",correct:false},
-            {text:"Giraffe",correct:false},
+        answers: [
+            { text: "Shark", correct: false },
+            { text: "Blue Whale", correct: true },
+            { text: "Elephant", correct: false },
+            { text: "Giraffe", correct: false },
         ]
-    },{
+    }, {
 
         question: "Which planet is known as the Red Planet?",
-        answers:[
-            {text:"Earth",correct:false},
-            {text:"Mars",correct:true},
-            {text:"Jupiter",correct:false},
-            {text:"Venus",correct:false},
+        answers: [
+            { text: "Earth", correct: false },
+            { text: "Mars", correct: true },
+            { text: "Jupiter", correct: false },
+            { text: "Venus", correct: false },
         ]
-    },{
+    }, {
         question: "What is the capital of France?",
-        answers:[
-            {text:"London",correct:false},
-            {text:"Berlin",correct:false},
-            {text:"Paris",correct:true},
-            {text:"Madrid",correct:false},
+        answers: [
+            { text: "London", correct: false },
+            { text: "Berlin", correct: false },
+            { text: "Paris", correct: true },
+            { text: "Madrid", correct: false },
         ]
-    },{
+    }, {
         question: "What is the tallest mountain in the world?",
-        answers:[
-            {text:"K2",correct:false},
-            {text:"Kilimanjaro",correct:false},
-            {text:"Mount Everest",correct:true},
-            {text:"Denali",correct:false},
+        answers: [
+            { text: "K2", correct: false },
+            { text: "Kilimanjaro", correct: false },
+            { text: "Mount Everest", correct: true },
+            { text: "Denali", correct: false },
         ]
-    },{
+    }, {
         question: "How many continents are there?",
-        answers:[
-            {text:"5",correct:false},
-            {text:"6",correct:false},
-            {text:"7",correct:true},
-            {text:"8",correct:false},
+        answers: [
+            { text: "5", correct: false },
+            { text: "6", correct: false },
+            { text: "7", correct: true },
+            { text: "8", correct: false },
         ]
     }
 ];
@@ -47,88 +47,111 @@ const questions =[
 const questionElement = document.getElementById("question");
 const answerButtons = document.getElementById("answer-button");
 const NextButton = document.getElementById("next");
+const restartButton = document.getElementById("restart");
 
-let currentQuestion=0;
-let scrore=0;
+let currentQuestion = 0;
+let scrore = 0;
+let timer;
 
-function startQuiz(){
-    currentQuestion=0;
-    scrore=0;
-    NextButton.innerHTML="Next";
+function startQuiz() {
+    currentQuestion = 0;
+    scrore = 0;
+    NextButton.innerHTML = "Next";
     showQuestion();
 
 }
 
-function  showQuestion(){
+function showQuestion() {
     resetState();
-    let current= questions[currentQuestion];
+    let current = questions[currentQuestion];
     let questionNo = currentQuestion + 1;
     questionElement.innerHTML = questionNo + ". " + current.question;
 
+    startTimer();
 
-    current.answers.forEach(answer=>{
-        const button= document.createElement("button");
-        button.innerHTML= answer.text;
+    current.answers.forEach((answer, index) => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
         button.classList.add("btn");
         answerButtons.appendChild(button);
-        if(answer.correct){
-            button.dataset.correct= answer.correct;
+        if (answer.correct) {
+            button.dataset.correct = answer.correct;
         }
-        button.addEventListener("click",selectAnswer)
-    })
+        button.addEventListener("click", selectAnswer)
+    });
 }
 
-function resetState(){
-    NextButton.style.display ="none";
-    while(answerButtons.firstChild){
+function resetState() {
+    clearInterval(timer);
+    NextButton.style.display = "none";
+    while (answerButtons.firstChild) {
         answerButtons.removeChild(answerButtons.firstChild);
     }
 
 }
 
-function selectAnswer(e){
-   const selectedBtn= e.target;
-   const isCorrect= selectedBtn.dataset.correct === "true";
-   if(isCorrect){
-    selectedBtn.classList.add("correct");
-    scrore++;
-   }else{
-    selectedBtn.classList.add("incorrect");
-   }
-   Array.from(answerButtons.children).forEach(button=>{
-    if(button.dataset.correct === "true"){
-        button.classList.add("correct");
+function selectAnswer(e) {
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+    if (isCorrect) {
+        selectedBtn.classList.add("correct");
+        scrore++;
+    } else {
+        selectedBtn.classList.add("incorrect");
     }
-    button.disabled = true;
+    Array.from(answerButtons.children).forEach((button) => {
+        if (button.dataset.correct === "true") {
+            button.classList.add("correct");
+        }
+        button.disabled = true;
 
-   });
-   NextButton.style.display = "block"
+    });
+    clearInterval(timer);
+    NextButton.style.display = "block"
 }
 
-function showScore(){
+function showScore() {
     resetState();
     questionElement.innerHTML = `You scored ${scrore} out of ${questions.length}!`;
-    NextButton.innerHTML= "Play Again";
-    NextButton.style.display="block";
+    // NextButton.innerHTML = "Play Again";
+    // NextButton.style.display = "block";
+    restartButton.style.display = "block";
+}
+
+function startTimer() {
+    let timeLeft = 10; 
+    const timerElement = document.createElement("div");
+    timerElement.id = "timer";
+    answerButtons.appendChild(timerElement);
+
+    timer = setInterval(function () {
+        timerElement.textContent = `Time Left: ${timeLeft} s`;
+        if (timeLeft <= 0) {
+            clearInterval(timer);
+            selectAnswer({ target: null });
+        }
+        timeLeft--;
+    }, 1000); 
 }
 
 
-
-function handlednextButton(){
+function handlednextButton() {
     currentQuestion++;
-    if(currentQuestion < questions.length){
-       showQuestion();
-    }else{
+    if (currentQuestion < questions.length) {
+        showQuestion();
+    } else {
         showScore();
     }
 
 }
 
-NextButton.addEventListener("click",()=>{
-    if(currentQuestion < questions.length){
+NextButton.addEventListener("click", () => {
+    if (currentQuestion < questions.length) {
         handlednextButton();
-    }else{
+    } else {
         startQuiz();
     }
 })
+restartButton.addEventListener("click", startQuiz);
+
 startQuiz();
